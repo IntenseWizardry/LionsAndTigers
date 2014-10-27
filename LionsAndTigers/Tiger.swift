1cab5a9390f8f24292c2c5284be38b16825f6413
Abed Alsolaiman <AbedAlsolaiman@Abeds-MacBook-Pro.local>
2014-10-27 07:44:00 +0200

    Having weird image resizing bug.

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Abed Alsolaiman on 10/20/14.
+//  Copyright (c) 2014 Abed Alsolaiman. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
